8f7ad7a9d5109cb7fda32c2d6f48b365140e1fc2
Alejandro Mateo <alexmateoweb3#gmail.com>
2023-04-21 18:26:09 +0200

    third Commit

---
+++ ChatSignalRCoreApp/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalIr
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(int room, string user, string mensaje)
+        {
+            //Enviamos un mensaje de forma asincrona a un grupo determinado
+            await Clients.Group(room.ToString()).SendAsync("RecibirMensaje", user, mensaje);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            //Añadimos a la persona a la sala
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+        }
+    }
+}
---
+++ ChatSignalRCoreApp/Controllers/ChatsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalIr.Controllers
+{
+    public class ChatsController : Controller
+    {
+        public static Dictionary<int, string> ListaChats = new Dictionary<int, string>()
+        {
+            {1, "Tecnologia" },
+            {2, "Deporte" },
+            {3, "Noticias" }
+        };
+
+        public IActionResult ListaDeChats()
+        {
+            return View(ListaChats);
+        }
+
+        public IActionResult Chat(int idChat)
+        {
+            return View("Chat", idChat);
+        }
+    }
+}
